82cfd74ad0272be4a9538361e2fa4364b3027feb
globalstone <baramyeun@gmail.com>
2024-05-01 19:46:49 +0900

    05.01 Server Deployment COM

--- src/main/java/com/model2/mvc/Application.java
+++ src/main/java/com/model2/mvc/Application.java
@@ -1,13 +1,19 @@
-package com.model2.mvc;
+////package com.model2.mvc;
+////
+////import org.springframework.boot.SpringApplication;
+////import org.springframework.boot.autoconfigure.SpringBootApplication;
+////
+////@SpringBootApplication
+////public class Application {
+////
+////	public static void main(String[] args) {
+////		SpringApplication.run(Application.class, args);
+////	}
+////
+////}
+//package com;
+//
+//////
+//package com;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-}
